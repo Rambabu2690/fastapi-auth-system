c674772b54fc030c01d1d9643c24949708137a76
rambabu <rambabu@gmail.com>
2026-01-07 16:34:43 +0530

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routes.auth_routes import router as auth_router
-from routes.user_routes import router as user_router
+
 
 app = FastAPI()
 @app.get("/")
@@ -11,4 +11,4 @@ def root():
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
-app.include_router(user_router, prefix="/api", tags=["Users"])
+
